dc2b3330cb7ffd160414a6e6c1dbdd8dbd086dc7
rlckdur123 <hello.rlckdur123@naver.com>
2022-12-16 18:06:22 +0900

    프로필 페이지

--- Muglang/src/main/java/com/muglang/muglangspace/controller/UserController.java
+++ Muglang/src/main/java/com/muglang/muglangspace/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.muglang.muglangspace.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.muglang.muglangspace.service.mglguser.MglgUserService;
 
@@ -12,4 +14,12 @@ public class UserController {
 	@Autowired
 	private MglgUserService mglgUserService;
 	//계정 관련 컨트롤
+	
+	@GetMapping("/profile")
+	public ModelAndView profileView() {
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("profile.html");
+		return mv;
+	}
 }
