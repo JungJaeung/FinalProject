ecc195d4906ed34bc1e6e86b9580b14d8316dccc
rlckdur123 <hello.rlckdur123@naver.com>
2022-12-20 09:56:10 +0900

    화면 연결

--- Muglang/src/main/java/com/muglang/muglangspace/service/mglgpost/MglgPostService.java
+++ Muglang/src/main/java/com/muglang/muglangspace/service/mglgpost/MglgPostService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.muglang.muglangspace.common.Function;
+
 import com.muglang.muglangspace.dto.MglgPostDTO;
 import com.muglang.muglangspace.entity.MglgPost;
 
