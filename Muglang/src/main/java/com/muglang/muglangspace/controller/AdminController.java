422e729f540054912a0307017e635a879949c90f
rlckdur123 <hello.rlckdur123@naver.com>
2022-12-20 11:45:15 +0900

    Revert "Revert "화면 연결""
    This reverts commit f9a352dfec924fbed01d1762c0159a508a9cd6d5.

--- Muglang/src/main/java/com/muglang/muglangspace/controller/AdminController.java
+++ Muglang/src/main/java/com/muglang/muglangspace/controller/AdminController.java
@@ -18,44 +18,65 @@ import com.muglang.muglangspace.service.mglgadmin.AdminService;
 public class AdminController {
 	@Autowired
 	private AdminService adminService;
-	
-	//어드민페이지로 이동
+
+	// 어드민페이지로 이동
 	@GetMapping("/adminView")
 	public ModelAndView adminView() {
 		ModelAndView mv = new ModelAndView();
-		System.out.println("asd");
-		mv.setViewName("/admin/admin.html");
+		mv.setViewName("/admin/memberManager.html");
 		return mv;
 	}
-	//밴 유저 yn 변경
-		@PutMapping("banUser")
-		public ResponseEntity<?> banUser(MglgUserDTO userDTO) {
-			MglgResponseDTO<MglgUserDTO> response = new MglgResponseDTO<>();
-			
-			try {
-				MglgUser user = MglgUser.builder()
-						.userId(userDTO.getUserId())
-						.userBanYn(userDTO.getUserBanYn())
-						.build();
-				user = adminService.uptUserBan(user);
-				MglgUserDTO returnUserDTO = MglgUserDTO.builder()
-													   .userId(user.getUserId())
-													   .userBanYn(user.getUserBanYn())
-													   .build();
-					
-				response.setItem(returnUserDTO);
-				return ResponseEntity.ok().body(response);
-			} catch (Exception e) {
-				response.setErrorMessage(e.getMessage());
-				return ResponseEntity.badRequest().body(response);
-			}
-			
-			
-			
 
-			
-			
-			
+	// 신고멤버로 이동
+	@GetMapping("/declarationMember")
+	public ModelAndView declarationMember() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/admin/declarationMember.html");
+		return mv;
+	}
+
+	// 신고글로 이동
+	@GetMapping("/declarationBoard")
+	public ModelAndView declarationBoard() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/admin/declarationBoard.html");
+		return mv;
+	}
+
+	// 신고댓글로 이동
+	@GetMapping("/declarationComment")
+	public ModelAndView declarationComment() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/admin/declarationComment.html");
+		return mv;
+	}
+
+	// FAQ로 이동
+	@GetMapping("/FAQ")
+	public ModelAndView FAQ() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/admin/F.A.Q.html");
+		return mv;
+	}
+
+	// 밴 유저 yn 변경
+	@PutMapping("banUser")
+	public ResponseEntity<?> banUser(MglgUserDTO userDTO) {
+		MglgResponseDTO<MglgUserDTO> response = new MglgResponseDTO<>();
+
+		try {
+			MglgUser user = MglgUser.builder().userId(userDTO.getUserId()).userBanYn(userDTO.getUserBanYn()).build();
+			user = adminService.uptUserBan(user);
+			MglgUserDTO returnUserDTO = MglgUserDTO.builder().userId(user.getUserId()).userBanYn(user.getUserBanYn())
+					.build();
+
+			response.setItem(returnUserDTO);
+			return ResponseEntity.ok().body(response);
+		} catch (Exception e) {
+			response.setErrorMessage(e.getMessage());
+			return ResponseEntity.badRequest().body(response);
 		}
+
+	}
 
 }
