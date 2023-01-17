f278e58962ed59ecdbd2b3a2e5ad721c0d0179ee
JungJaeung <hello.git.jk3576350@naver.com>
2023-01-17 17:50:15 +0900

    삭제 쿼리 활성화를 위한 임포트 변경.

--- Muglang/src/main/java/com/muglang/muglangspace/repository/MglgRestaurantRepository.java
+++ Muglang/src/main/java/com/muglang/muglangspace/repository/MglgRestaurantRepository.java
@@ -2,8 +2,8 @@ package com.muglang.muglangspace.repository;
 
 import javax.transaction.Transactional;
 
-import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
