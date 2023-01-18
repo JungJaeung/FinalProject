d2bae05d65a7635a02ab88f7b9204db91b2a4438
rlckdur123 <rlckdur123@naver.com>
2023-01-18 20:04:03 +0900

    팔로잉 post작성

--- Muglang/src/main/resources/static/assets/js/like.js
+++ Muglang/src/main/resources/static/assets/js/like.js
@@ -5,9 +5,6 @@ $.like_button = function(loginUserId) {
 		postId = e.target.id;
 		postId = Number(postId);
 		if ($(e.target).css("color") == "rgb(0, 0, 0)") {
-			console.log(loginUserId);
-			console.log("좋아요 누른 포스트 id: " + postId + "좋아요 누른 유저 : " + loginUserId);
-			console.log("타입 : " + typeof(postId));
 			
 			$(e.target).css("color", "red");
 			//좋아요를 눌렀을때 ajax
@@ -21,7 +18,7 @@ $.like_button = function(loginUserId) {
 					postId: postId
 				},
 				success: function(obj) {
-					console.log(obj)
+					console.log("포스트 " + postId + "번 좋아요 " + obj + "개")
 					//좋아요 개수를 likeCnt +아이디 로 만들었음
 
 					$("#likeCnt" + postId).html(obj);
@@ -32,7 +29,6 @@ $.like_button = function(loginUserId) {
 			});
 		} else {
 			$(e.target).css("color", "black");
-			console.log("좋아요 해제한 포스트 id: " + postId);
 			$.ajax({
 				url: '/post/likeDown',
 				type: 'get',
@@ -41,7 +37,7 @@ $.like_button = function(loginUserId) {
 					postId: postId
 				},
 				success: function(obj) {
-					console.log(obj)
+					console.log("좋아요 해제한 포스트 id: " + postId);
 					//좋아요 개수를 likeCnt +아이디 로 만들었음
 					$("#likeCnt" + postId).html(obj);
 				},
